03b3a2af9b90bc5cebe9b91d7f0a40b72147c49c
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-21 02:45:42 +0300

    Create new post, add posts listing & restrict post creation to signed in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, except: [:index]  
+
   def index
+    @posts=Post.all
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,25 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]  
   def index 
-
+    @posts=Post.all
   end
 
   def new 
-
+    @post = current_user.posts.build
   end
 
   def create 
-
+    @post = current_user.posts.build(post_params)
+    if(@post.save)
+      flash[:success] = "Your post was successfully created"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
+
+  private
+   def post_params
+    params.require(:post).permit(:content)
+   end
 end
