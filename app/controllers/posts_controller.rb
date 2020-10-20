4b39dc958ba7181e7448c5153aa740f5cd7e65a4
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-21 01:21:29 +0300

    Generate create post migration, add  post model, posts controller, views and routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+  def index 
+
+  end
+
+  def new 
+
+  end
+
+  def create 
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_211736) do
+ActiveRecord::Schema.define(version: 2020_10_20_221641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
