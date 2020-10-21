d43ae1fd150261cb01a3150df6cad5e1046251d9
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-21 12:51:47 +0300

    Add partial and helper for flash messages, style the navbar

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -6,4 +6,21 @@ module SiteHelper
       render '/layouts/guest_menu'
     end
   end
+
+  def member_or_guest_action
+    if user_signed_in? 
+      render '/layouts/member_action'
+    else
+      render '/layouts/guest_action'
+    end
+  end
+
+  def flash_message(name)
+    if name=='notice'
+      render 'layouts/notice'
+    elsif name=='alert'
+      render 'layouts/alert'
+    end
+  end
+
 end
