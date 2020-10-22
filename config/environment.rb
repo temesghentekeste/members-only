30f8bb6ddc9edd6c34846a8e93c7be9d03587cd8
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-22 22:55:22 +0300

    Add additional sendgrid configurations

--- config/environment.rb
+++ config/environment.rb
@@ -11,6 +11,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
+  :domain => 'membersonly-temesghen.herokuapp.com',
   :enable_starttls_auto => true
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'membersonly-temesghen.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'membersonly-temesghen.herokuapp.com'}
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
