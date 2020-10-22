79bededa80a4f973cf7b3b3b3c4e456575405956
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-23 00:01:01 +0300

    Disable Devise confirmable email

---
+++ db/migrate/20201022205348_remove_columns_from_users.rb
@@ -0,0 +1,8 @@
+class RemoveColumnsFromUsers < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :users, :confirmation_token
+    remove_column :users, :confirmed_at
+    remove_column :users, :confirmation_sent_at
+    remove_column :users, :unconfirmed_email
+  end
+end
